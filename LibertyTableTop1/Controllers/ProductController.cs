f23584c1a92f8305b979b73aeddd185ce9a9dad3
patil <patil@HP_LAPTOP>
2025-02-20 22:14:36 -0500

    Add project files.

---
+++ LibertyTableTop1/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using LibertyTableTop1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibertyTableTop1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Dining Table", Price = 500 },
+            new Product { Id = 2, Name = "Coffee Table", Price = 300 },
+            new Product { Id = 3, Name = "Office Desk", Price = 700 },
+            new Product { Id = 4, Name = "Study Table", Price = 400 },
+            new Product { Id = 5, Name = "Console Table", Price = 350 }
+        };
+            return View(products);
+        }
+    }
+}
---
+++ LibertyTableTop1/README.txt
@@ -0,0 +1,7 @@
+﻿LibertyTableTop
+Web application
+2025-02-18
+
+
+Prajwal Patil
+12:00 PM Created the project named LibertyTableTop.
